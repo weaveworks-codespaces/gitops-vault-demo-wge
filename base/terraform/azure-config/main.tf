7a8a86e2569374138e74a2d342f518a6987a9080
James Wilson <jwils22@gmail.com>
2022-11-30 22:25:51 -0500

    Fixed tf provider for azure

--- base/terraform/azure-config/main.tf
+++ base/terraform/azure-config/main.tf
@@ -5,6 +5,15 @@ terraform {
       version = "~>2.0"
     }
   }
+
+provider "azurerm" {
+  features {}
+
+  subscription_id   = "${env.ARM_SUBSCRIPTION_ID}"
+  tenant_id         = "${env.ARM_TENANT_ID}"
+  client_id         = "${env.ARM_CLIENT_ID}"
+  client_secret     = "${env.ARM_CLIENT_SECRET}"
+}
   
   resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
