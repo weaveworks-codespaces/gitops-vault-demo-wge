c36268967e31418d95c9cd27e1092508b0d22ff9
James Wilson <jwils22@gmail.com>
2022-11-30 23:04:17 -0500

    Fixed typos in tf files

--- base/terraform/azure-config/main.tf
+++ base/terraform/azure-config/main.tf
@@ -5,15 +5,10 @@ terraform {
       version = "~>2.0"
     }
   }
+}
 
 provider "azurerm" {
   features {}
-
-  subscription_id   = "${env.ARM_SUBSCRIPTION_ID}"
-  tenant_id         = "${env.ARM_TENANT_ID}"
-  client_id         = "${env.ARM_CLIENT_ID}"
-  client_secret     = "${env.ARM_CLIENT_SECRET}"
-  use_msi = true
 }
   
   resource "random_pet" "rg_name" {
